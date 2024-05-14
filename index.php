416ade2658658984446bb00d1c3d112332a46c50
JoyKings <wolkodaw11@spaces.ru>
2024-05-14 21:39:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 
+<?php
+
+define('TOKEN', '6955205284:AAEfiOcVT4EHPguedfVpqBriD7npsmXejKs');
+
+$update = json_decode(file_get_contents('php://input'), TRUE);
+
+// Получаем id чата
+$chat_id = $update['message']['chat']['id'];
+
+// Получаем текст сообщения
+$text = $update['message']['text'];
+
+// Отправляем ответ на сообщение
+if($text == 'привет' || $text == 'здравствуйте') {
+    sendMessage('Приветствую вас!', $chat_id);
+}
+
+// Функция для отправки сообщения
+function sendMessage($message, $chat_id) {
+    file_get_contents('https://api.telegram.org/bot'.TOKEN.'/sendMessage?chat_id='.$chat_id.'&text='.urlencode($message));
+}
